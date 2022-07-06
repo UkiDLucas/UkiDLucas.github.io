4123bbd9301b9bb97d3e6151083e405757d2e3d1
Uki D. Lucas <UkiDLucas@gmail.com>
2022-07-06 19:51:50 -0400

    paper folds example

---
+++ content/Julia/Brilliant/half_way_job.jl
@@ -0,0 +1,30 @@
+### A Pluto.jl notebook ###
+# v0.19.9
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 4c6ec0e4-fd86-11ec-270c-d139c5bcf141
+md"""
+### This will the the challenge for the next day!
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─4c6ec0e4-fd86-11ec-270c-d139c5bcf141
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
